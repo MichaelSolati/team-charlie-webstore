e93471d03b472042d1e28ed62f5cf01eb9cc193c
Antonio <psicofrost@gmail.com>
2016-11-08 18:44:15 -0500

    Payment Method

---
+++ imports/app/paymentmethod/paymentmethod.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+import template from "./payment.component.html";
+
+@Component({
+  selector: "app-payment",
+  template: template
+})
+export class PaymentComponent {
+  constructor () {
+
+  }
+}
