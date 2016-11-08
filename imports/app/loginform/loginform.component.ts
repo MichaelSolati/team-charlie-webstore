5bd4beeaa434a411d43b1ff7f750b85fe8513c21
hanniblejulien <hanniblejulien@gmail.com>
2016-11-08 18:38:32 -0500

    loginform hannible

---
+++ imports/app/loginform/loginform.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+import template from "./loginform.component.html";
+
+@Component({
+  selector: "app-loginform",
+  template: template
+})
+export class LoginFormComponent {
+  constructor () {
+
+  }
+}
