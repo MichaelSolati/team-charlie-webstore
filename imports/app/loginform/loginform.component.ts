7a9ad7c23a554c62af9fc96e3a1da3bfb3303206
Michael Solati <mkslt04@gmail.com>
2016-11-15 18:07:54 -0500

    Create basic sign in/sign up

--- imports/app/loginform/loginform.component.ts
+++ imports/app/loginform/loginform.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { Accounts } from "meteor/accounts-base";
+import { Meteor } from "meteor/meteor";
 
 import template from "./loginform.component.html";
 
@@ -7,7 +9,56 @@ import template from "./loginform.component.html";
   template: template
 })
 export class LoginFormComponent {
+  private signInForm: any = {
+    email:"",
+    password:"",
+    profile: {
+      name: ""
+    }
+  }
+  private isSigningIn: boolean = true;
   constructor () {
 
+  }
+  private signIn() {
+    Meteor.loginWithPassword(this.signInForm.email, this.signInForm.password, (error, success) => {
+      if (error) {
+        Bert.alert("Could not sign in: "+error.reason, "danger", "growl-top-right");
+      } else {
+        Bert.alert("Welcome Back!", "success", "growl-top-right");
+      }
+    })
+  }
+
+  private signOut() {
+    Meteor.logout((error, success) => {
+      if (error) {
+        Bert.alert("Could not sign out: "+error.reason, "danger", "growl-top-right");
+      } else {
+        Bert.alert("Good riddance!", "success", "growl-top-right");
+      }
+    })
+  }
+
+  private signUp() {
+    Accounts.createUser(this.signInForm, (error, success) => {
+      if (error) {
+        Bert.alert("Could not create account: "+error.reason, "danger", "growl-top-right");
+      } else {
+        Bert.alert("Welcome!", "success", "growl-top-right");
+      }
+    });
+  }
+
+  private submit() {
+    if (this.isSigningIn) {
+      this.signIn();
+    } else {
+      this.signUp();
+    }
+  }
+
+  private toggleSignIn() {
+    this.isSigningIn = !this.isSigningIn;
   }
 }
