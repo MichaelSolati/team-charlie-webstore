03d17bab5a4c35771ec5605f168964e94e3f622e
James <2yankeefans2@gmail.com>
2016-11-15 19:39:14 -0500

    Connected Shipping Inputs to Database

--- imports/app/shipping/shipping.component.ts
+++ imports/app/shipping/shipping.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Meteor } from "meteor/meteor";
 
 import template from "./shipping.component.html";
 
@@ -7,7 +8,23 @@ import template from "./shipping.component.html";
   template: template
 })
 export class ShippingComponent {
+  private address :any = {
+    "address1":"",
+    "address2":"",
+    "city":"",
+    "zip":"",
+    }
   constructor () {
-
+  }
+  submit(){
+  console.log(this.address);
+  let user = {"_id":Meteor.userId()};
+  Meteor.users.update(user, {
+    $set : {
+      profile: {
+        address: this.address
+      }
+    }
+  })
   }
 }
