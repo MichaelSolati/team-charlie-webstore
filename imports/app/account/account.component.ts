249410bb8dd9b933a48819acfa2958054d0811d5
James <2yankeefans2@gmail.com>
2016-11-20 19:16:23 -0500

    Created Account Page

---
+++ imports/app/account/account.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+import template from "./account.component.html";
+
+@Component({
+  selector: "app-template",
+  template: template
+})
+export class AccountComponent {
+  constructor () {
+
+  }
+}
