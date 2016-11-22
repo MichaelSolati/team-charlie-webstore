7f45498041337d610928af452a32f50e26d151f9
Antonio <valderramaa1@southernct.edu>
2016-11-22 16:25:22 -0500

    Payment info

--- imports/app/paymentmethod/paymentmethod.component.ts
+++ imports/app/paymentmethod/paymentmethod.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-
+import {Meteor} from "meteor/meteor";
 import template from "./paymentmethod.component.html";
 
 @Component({
@@ -7,7 +7,24 @@ import template from "./paymentmethod.component.html";
   template: template
 })
 export class PaymentComponent {
+  private payment: any = {
+    "ccnumber" : "",
+    "month" : "",
+    "year" : "",
+    "csv" : "",
+    "zip" : ""
+  };
   constructor () {
-
+    console.log(this.payment);
+    Meteor.users.update({"_id" : Meteor.userId()}, {
+      $set: {
+        profile: {
+          creditcard: this.payment
+      }
+    }
+    });
+  }
+  submit() {
+    console.log(this.payment);
   }
 }
