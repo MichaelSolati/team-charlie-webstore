683a2ffed9f15acff16f092cd10476f78dcf8fa1
James <2yankeefans2@gmail.com>
2016-11-22 21:41:41 -0500

    Added a Checkout Component

---
+++ imports/app/checkout/checkout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+import template from "./template.component.html";
+
+@Component({
+  selector: "app-template",
+  template: template
+})
+export class TemplateComponent {
+  constructor () {
+
+  }
+}
